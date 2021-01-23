b8e283be79500941f7e48a9cea385c0cc12eb13a
Jonny Tullis <jon.tullis558@gmail.com>
2021-01-23 12:48:05 -0700

    Built HomePage

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import './HomePage.css'
+import {Grid, Container, Box, Button} from '@material-ui/core'
+import {colors} from '../../theme'
+import Logo from '../../assets/images/scattergories-logo.png'
+import { useHistory } from 'react-router-dom'
 
 export default function HomePage() {
+    const history = useHistory()
     return (
-        <h1>
-            Home Page
-        </h1>
+        <Container style={{ padding: `${window.innerHeight / 16}px 12px`, maxHeight: `${window.innerHeight}px` }}>
+            <Box display="flex">
+                <Box m="auto" style={{ boxShadow: '5px 8px 18px #888888', borderRadius: 50 }}>
+                    <img src={Logo} style={{ marginBottom: -5, height: 300 }}  alt="Logo" />
+                </Box>
+            </Box>
+            <Box display="flex" style={{ margin: '0px 0 24px 0' }}>
+                <Box m="auto">
+                    <h2>Let's play Scattergories!</h2>
+                </Box>
+            </Box>
+            <Grid container alignItems="center" spacing={3} style={{ textAlign: 'center' }}>
+                <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+                    <Button
+                        variant="contained"
+                        className="Button-Large"
+                        style={{ backgroundColor: colors.primary }}
+                        onClick={() => history.push('/games/host')}
+                    >
+                        Host Game
+                    </Button>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+                    <Button
+                        variant="contained"
+                        className="Button-Large"
+                        style={{ backgroundColor: colors.secondary }}
+                        onClick={() => history.push('/games/join')}
+                    >
+                        Join Game
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
